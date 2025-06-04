23a32fd1cc745f94961a95646dd99d9322007f79
Edoardo Lova <edoardolova81@gmail.com>
2025-06-04 14:50:39 +0200

    add conta vocali + arrow function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,35 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function getNumbVocals(word){
+    let numbVocals = 0;
+    const vocals = ["a","e","i","o","u"];
+    for(let i = 0; i < word.length; i++){
+        for(let j = 0; j < vocals.length; j++){
+            if (word[i].toLowerCase() === vocals[j]) {
+                numbVocals ++;
+            }
+        }
+    }
+    return numbVocals;
+}
 
+/* arrow function
+const getNumbVocals = word =>{
+    let numbVocals = 0;
+    const vocals = ["a","e","i","o","u"];
+    for(let i = 0; i < word.length; i++){
+        for(let j = 0; j < vocals.length; j++){
+            if (word[i].toLowerCase() === vocals[j]) {
+                numbVocals ++;
+            }
+        }
+    }
+    return numbVocals;
+}  */
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getNumbVocals(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
