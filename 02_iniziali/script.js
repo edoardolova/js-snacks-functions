a46fa1b159db88ca5c1defa60760a9a77f480401
Edoardo Lova <edoardolova81@gmail.com>
2025-06-04 14:31:47 +0200

    add iniziali solution + arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitials(names){
+    const initialsArr = [];
+    for (let i = 0; i < names.length; i++){
+        initialsArr.push(names[i][0]);
+    }
+    return initialsArr;
+}
 
-
+// arrow function 
+// const getInitials = names =>{
+//     const initialsArr = [];
+//     for (let i = 0; i < names.length; i++){
+//         initialsArr.push(names[i][0]);
+//     }
+//     return initialsArr;
+// }
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
